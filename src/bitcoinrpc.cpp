a5e384162f3ed68cb91fbe040fec504503727e44
nelisky <nelisky.btc@gmail.com>
2013-06-08 04:50:22 +0100

    importaddress last argument is bool

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -3270,6 +3270,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "createrawtransaction"   && n > 1) ConvertTo<Object>(params[1]);
     if (strMethod == "signrawtransaction"     && n > 1) ConvertTo<Array>(params[1]);
     if (strMethod == "signrawtransaction"     && n > 2) ConvertTo<Array>(params[2]);
+    if (strMethod == "importaddress"          && n > 2) ConvertTo<bool>(params[2]);
 
     return params;
 }
