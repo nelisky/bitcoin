022e9aa0f2a7714d77b66b3485e707cf92fe567f
terracoin <terracoin76@gmail.com>
2013-07-25 19:10:43 +0200

    removed non-validated checkpoint
     - block data not available on seednodes ; could also be malicious/wrong
     - exchanges are still relying on current chain......
     - commit d0088be15b removes the weird difficulty halving, at block
       175000+

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -91,12 +91,11 @@ namespace Checkpoints
        (107368, uint256("0x000000000009062fe1b4c0654b3d152282545aa8beba3c0e4981d9dfa71b1eaa"))
        (108106, uint256("0x00000000000282bf4e2bd0571c42165a67ffede3b81f2387e301369162107020"))
        (110197, uint256("0x0000000000002d863064910c8964f5d8e2883aca9760c19368fe043263e2bfdd"))
-       (165843, uint256("0x000000000000f0a91dd1b5db479092cd2f0fb9e664df66bf2f04734cfc4ec694"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1374719634, // * UNIX timestamp of last checkpoint block
-        344498,     // * total number of transactions between genesis and last checkpoint
+        1366364155, // * UNIX timestamp of last checkpoint block
+        209955,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         1500.0      // * estimated number of transactions per day after checkpoint
     };
